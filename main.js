2fd3158a70667cfb25937b4cf797652c5e701282
alph <capocr@narod.ru>
2020-06-16 22:10:29 +0300

    fix

--- main.js
+++ main.js
@@ -13,29 +13,22 @@ function makeTurn(battleState) {
   return battleOutput;
 }
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  terminal: false
-});
-
 function makeDraft(draftOptions) {
   const draft = { };
   return draft;
 }
 
-function playGame() {
-  rl.on('line', line => {
-    const obj = JSON.parse(line);
-    if (obj.hasOwnProperty('PlayerId')) {
-      console.log(JSON.stringify(makeDraft(obj)));
-    } else if (obj.hasOwnProperty("My"))
-    {
-      console.log(JSON.stringify(makeTurn(obj)));
-    }
-
-    playGame();
-  })
-}
-
-playGame();
+readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+}).on("line", line => {
+  const obj = JSON.parse(line);
+  if (obj.hasOwnProperty("PlayerId")) {
+    console.log(JSON.stringify(makeDraft(obj)));
+  } else if (obj.hasOwnProperty("My"))
+  {
+    console.log(JSON.stringify(makeTurn(obj)));
+  }
+})
 
